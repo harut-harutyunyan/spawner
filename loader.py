a839bafd3c01913849770d60ef7981e889fc0c3f
Harut Harutyunyan <harut@MacBook-Pro-Harut.local>
2023-10-21 23:55:58 +0400

    initial commit

---
+++ loader.py
@@ -0,0 +1,208 @@
+import os
+import re
+
+
+import pyseq
+
+# env
+os.environ["MY_WORK_ROOT"] = "/Users/harut/Documents/harut/prj"
+os.environ["MY_PROJECT_ABBR"] = "trainvfx"
+os.environ["MY_SEQUENCE"] = "int010"
+os.environ["MY_SHOT"] = "int010_0020"
+os.environ["MY_TASK_NAME"] = "scan"
+
+# example filepath
+filepath = "/Users/harut/Documents/harut/prj/trainvfx/int010/int010_0020/scan/fg01/v001/int010_0020_fg01.%04d.jpg"
+# example filelist
+# file_list = ['env:MY_WORK_ROOT', 'env:MY_PROJECT_ABBR', 'env:MY_SEQUENCE', 'env:MY_SHOT', 'scan', 'fg01', 'v001', 'int010_0020_fg01.1001.jpg']
+file_list = ['env:MY_WORK_ROOT', 'env:MY_PROJECT_ABBR', 're:.*', 're:.*', 'scan', 'fg01', 'v:all', r're:.*']
+
+
+WORK_ROOT = os.getenv("MY_WORK_ROOT", "")
+PROJECT_ABBR = os.getenv("MY_PROJECT_ABBR", "")
+
+class PathsHandler(object):
+
+
+    SEQ = True
+    FILE_FOLMATS = [
+                    ".jpg",
+                    ".png",
+                    ".exr",
+                    ".tif",
+                    ".tiff",
+                    ".dpx",
+                    ".hdr",
+                    ".targa",
+                    ".cin",
+                    ".abc",
+                    ".fbx",
+                    ".obj",
+                    ".gizmo",
+                    ".nk",
+                    ]
+
+    @classmethod
+    def _path_split(cls, filepath):
+        file_list = []
+
+        while True:
+            filepath, folder = os.path.split(filepath)
+            if folder:
+                file_list.insert(0, folder)
+            else:
+                break
+
+        return file_list
+
+    @classmethod
+    def _handle_file_name(cls, file_name):
+
+        name, ext = os.path.splitext(file_name)
+
+        if name.endswith("#") or re.search(r'.*%[0-9]+d$', name):
+            return r"re:\w+[\._]\d+\.{}$".format(ext[1:])
+
+        return r"re:.*\.{}$".format(ext[1:])
+
+    @classmethod
+    def _split(cls, filepath, guess=True):
+        file_list = cls._path_split(filepath)
+        file_list = file_list[file_list.index(PROJECT_ABBR)+1:]
+
+        file_list = ['env:MY_WORK_ROOT', 'env:MY_PROJECT_ABBR']+file_list
+
+        for string in file_list:
+            match = re.search(r'v(\d+)', string)
+            if match:
+                version = str(match.group())
+                if version in file_list:
+                    file_list[file_list.index(version)] = "v:latest"
+
+        file_list[-1] = cls._handle_file_name(file_list[-1])
+
+        if guess:
+            file_list[2] = "env:MY_SEQUENCE"
+            file_list[3] = "env:MY_SHOT"
+            file_list[4] = "env:MY_TASK_NAME"
+
+        return file_list
+
+    @classmethod
+    def _get_version(cls, version_list, index=0):
+        pattern = r'v(\d+)'
+
+        all_versions = [0]
+
+        for version in version_list:
+            match = re.search(pattern, version)
+            if match:
+                version_number = int(match.group(1))
+                all_versions.append(version_number)
+
+        return sorted(all_versions, reverse=True)[index%len(all_versions)]
+
+    @classmethod
+    def _handle_env(cls, string):
+        return os.getenv(string[4:], "")
+
+    @classmethod
+    def _handle_regex(cls, filepath, string):
+        pattern = string[3:]
+        match_list = []
+
+        if not os.path.isdir(filepath):
+            return []
+
+        for item in os.listdir(filepath):
+            if re.search(pattern, item):
+                match_list.append(item)
+
+        return match_list
+
+    @classmethod
+    def _handle_version(cls, filepath, string):
+        ver = string[2:]
+        if ver.isdigit():
+            return cls._handle_regex(filepath, r"re:^v.*{}$".format(ver))
+        elif ver=="all":
+            return cls._handle_regex(filepath, r"re:v(\d+)")
+        elif ver=="latest":
+            latest_version = cls._get_version(os.listdir(filepath))
+            return cls._handle_version(filepath, "v:{}".format(latest_version))
+        elif ver.startswith("-") and ver[1:].isdigit():
+            version = cls._get_version(os.listdir(filepath), int(ver[1:]))
+            return cls._handle_version(filepath, "v:{}".format(version))
+        else:
+            return []
+
+    @classmethod
+    def _construct_file_name(cls, string):
+        pass
+
+    @classmethod
+    def _construct_file_paths(cls, string_list, current_path=""):
+        if not string_list:
+            return [current_path]
+
+        string, *tail = string_list
+        results = []
+
+        if string.startswith("env:"):
+            new_path = os.path.join(current_path, cls._handle_env(string))
+            if os.path.exists(new_path):
+                results.extend(cls._construct_file_paths(tail, new_path))
+
+        if string.startswith("re:"):
+            for folder in cls._handle_regex(current_path, string):
+                new_path = os.path.join(current_path, folder)
+                if os.path.exists(new_path):
+                    results.extend(cls._construct_file_paths(tail, new_path))
+
+        if string.startswith("v:"):
+            for folder in cls._handle_version(current_path, string):
+                new_path = os.path.join(current_path, folder)
+                if os.path.exists(new_path):
+                    results.extend(cls._construct_file_paths(tail, new_path))
+
+        else:
+            new_path = os.path.join(current_path, string)
+            if os.path.exists(new_path):
+                results.extend(cls._construct_file_paths(tail, new_path))
+
+        return results
+
+    @classmethod
+    def _pyseq_convert(cls, path_dict):
+        if cls.SEQ:
+            for i in path_dict.keys():
+                path_dict[i] = pyseq.Sequence(path_dict[i])
+
+        return path_dict
+
+    @classmethod
+    def construct_file_paths(cls, string_list):
+        file_paths = list(set(cls._construct_file_paths(string_list)))
+        path_dict = {}
+        for path in file_paths:
+            if not os.path.splitext(path)[1] in cls.FILE_FOLMATS:
+                continue
+            dirname = os.path.dirname(path)
+            if not dirname in path_dict.keys():
+                path_dict[dirname] = [path]
+                continue
+            path_dict[dirname].append(path)
+
+        return cls._pyseq_convert(path_dict)
+
+if __name__ == '__main__':
+
+    file_list = PathsHandler._split(filepath)
+    print(file_list)
+    # PathsHandler.SEQ = False
+
+    dd = PathsHandler.construct_file_paths(file_list)
+
+    for i in dd.keys():
+        print(dd[i])
+
---
+++ spawner.py
@@ -0,0 +1,181 @@
+import os
+import re
+
+
+import nuke
+from loader import PathsHandler
+import pyseq
+
+
+class SpawnerNode(object):
+
+    CLASS = "NoOp"
+
+    @classmethod
+    def create(cls):
+        node = nuke.createNode(cls.CLASS)
+        node.setName("Spawner1")
+        node.knob("tile_color").setValue(1280068863)
+
+        knob = nuke.Tab_Knob("Spawner")
+        node.addKnob(knob)
+        knob = nuke.File_Knob("file", "file")
+        node.addKnob(knob)
+        knob = nuke.PyScript_Knob("analize", "analize", "import spawner\nspawner.SpawnerNode.analize()")
+        knob.clearFlag(nuke.STARTLINE)
+        node.addKnob(knob)
+        knob = nuke.Boolean_Knob("delete_line", "delete after")
+        knob.setFlag(nuke.STARTLINE)
+        node.addKnob(knob)
+        knob = nuke.Boolean_Knob("guess", "from env")
+        knob.clearFlag(nuke.STARTLINE)
+        knob.setValue(1)
+        node.addKnob(knob)
+        knob = nuke.Text_Knob("div_01", "")
+        node.addKnob(knob)
+        knob = nuke.Boolean_Knob("delete_after", "delete spawner")
+        node.addKnob(knob)
+        knob = nuke.Boolean_Knob("select_after", "select node")
+        knob.clearFlag(nuke.STARTLINE)
+        node.addKnob(knob)
+        knob = nuke.XY_Knob("position_after", "position")
+        node.addKnob(knob)
+        knob.setValue([0, -100])
+        knob = nuke.Text_Knob("div_02", "")
+        node.addKnob(knob)
+
+        return node
+
+    @classmethod
+    def analize(cls):
+        node = nuke.thisNode()
+        file_path = node.knob("file").value()
+        if not os.path.splitext(file_path)[1] in PathsHandler.FILE_FOLMATS:
+            file_path = None
+
+        delete_after = node.knob("delete_line").getValue()
+        guess = node.knob("guess").getValue() == 1.0
+
+        [node.removeKnob(node[n]) for n in node.knobs() if n.startswith("input_")]
+
+        if file_path:
+            knob = nuke.Boolean_Knob("input_is_seq", "image sequence")
+            knob.setValue(1)
+            node.addKnob(knob)
+
+            path_list = PathsHandler._split(file_path, guess)
+            for i, string in enumerate(path_list):
+                knob = nuke.String_Knob("input_{}".format(i), "search".format(i), string)
+                node.addKnob(knob)
+            knob = nuke.Text_Knob("input_div", "")
+            node.addKnob(knob)
+            knob = nuke.Multiline_Eval_String_Knob("input_post_script", "post spawn script")
+            node.addKnob(knob)
+            knob = nuke.PyScript_Knob("input_spawn", "Spawn!", "import spawner\nspawner.SpawnerNode.spawn()")
+            knob.setFlag(nuke.STARTLINE)
+            node.addKnob(knob)
+
+            if delete_after:
+                node.removeKnob(node.knob("file"))
+                node.removeKnob(node.knob("analize"))
+                node.removeKnob(node.knob("delete_line"))
+                node.removeKnob(node.knob("guess"))
+                node.removeKnob(node.knob("div_01"))
+        else:
+            nuke.message("wrong filepath!\n\n    - must not be empty\n    - must be a file")
+
+    @classmethod
+    def _collect_search_inputs(cls):
+        node = nuke.thisNode()
+
+        knobs = [node[n].value() for n in node.knobs() if re.match(r'^input_\d+$', n)]
+
+        input_data = {
+            "file_list": knobs,
+            "seq": node["input_is_seq"].getValue() == 1.0,
+            "node": node,
+        }
+        return input_data
+
+    @classmethod
+    def spawn(cls):
+        input_data = cls._collect_search_inputs()
+        file_list = input_data["file_list"]
+        is_seq = input_data["seq"]
+        spawner = input_data["node"]
+
+        PathsHandler.SEQ = is_seq
+
+        files_dict = PathsHandler.construct_file_paths(file_list)
+        spawned = []
+        for folder, content in files_dict.items():
+            if isinstance(content, list):
+                _, ext = os.path.splitext(content[0])
+            else:
+                _, ext = os.path.splitext(str(content))
+
+            if ext in PathsHandler.FILE_FOLMATS[:9]:
+                if is_seq:
+                    if content.length() > 1:
+                        seq = "{} {}".format(content.format("%h#%t"), content.format("%s-%e"))
+                    else:
+                        seq = "{} {}".format(str(content), "1-1")
+
+                    read_node = nuke.createNode('Read', inpanel=False)
+                    read_node.knob("file").fromUserText(os.path.join(folder, seq))
+                    spawned.append(read_node)
+                else:
+                    for seq in nuke.getFileNameList(folder):
+                        if os.path.splitext(seq.rsplit(" ", 1)[0])[1] in PathsHandler.FILE_FOLMATS:
+                            read_node = nuke.createNode('Read')
+                            read_node.knob('file').fromUserText(os.path.join(folder, seq))
+                            spawned.append(read_node)
+
+            elif ext in PathsHandler.FILE_FOLMATS[9:12]:
+                if is_seq:
+                    read_node = nuke.createNode('ReadGeo2', inpanel=False)
+                    read_node.knob("file").setValue(os.path.join(folder, str(content)))
+                    spawned.append(read_node)
+
+            elif ext in PathsHandler.FILE_FOLMATS[12:]:
+
+                if is_seq:
+                    nuke.nodePaste(os.path.join(folder, str(content)))
+                else:
+                    for c in content:
+                        nuke.nodePaste(os.path.join(c))
+                # delete
+                if spawner["delete_after"].getValue() == 1.0:
+                    nuke.delete(spawner)
+                return
+
+        for node in spawner.dependent(nuke.INPUTS):
+            node.setInput(0, spawned[0])
+
+        # position nodes and selection
+        position = spawner["position_after"].getValue()
+        select = spawner["select_after"].getValue() == 1.0
+        offset_x = 0
+        offset_y = 0
+        for node in spawned:
+            node.setSelected(select)
+            node.setXYpos(int(spawner.xpos()+position[0]+offset_x), int(spawner.ypos()+position[1]+offset_y))
+            if position[0] != 0:
+                offset_x += 100
+            if position[1] != 0:
+                offset_x += 100
+
+        # run post script
+        script = spawner["input_post_script"].value()
+        if script != "":
+            post_script = """
+spawned = [nuke.toNode(n) for n in "{}".split(",")]
+
+""".format(",".join([n.name() for n in spawned]))
+
+            post_script += script
+            exec(post_script)
+
+        # delete
+        if spawner["delete_after"].getValue() == 1.0:
+            nuke.delete(spawner)
